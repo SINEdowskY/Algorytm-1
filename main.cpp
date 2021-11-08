ee631427e6535252fa5742d886be08e0f93fde88
SINEdowskY <endygra@gmail.com>
2021-11-08 11:59:07 +0100

    optimalized algorithm 2

--- main.cpp
+++ main.cpp
@@ -36,13 +36,13 @@ vector<int> selection_v1(vector<int> arr){
 
 //algorytm 1 - realizacja poprzez swap()
 vector<int> selection_v2(vector<int> arr){
-    int count = 1;
+    int count = 1; // ilosc wykonan funkcji swap()
     for(int i = 0; i < arr.size()-1; i++){
         if(arr[i] < 0){
             if(arr[i+1] < 0){
                 count += 1;
             }
-            if(arr[i+1] >= 0  && i < arr.size()-1) {
+            else if(arr[i+1] >= 0){
                 for(int j = 0; j < count; j++){
                     swap(arr[i-j],arr[i+1-j]);
                 }
